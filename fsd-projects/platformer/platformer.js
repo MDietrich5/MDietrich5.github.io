0f2d440ad8040e94b47c117a2cc4e26c4a654cdb
MDietrich5 <mdietrich805@gmail.com>
2026-02-11 14:37:44 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -1,5 +1,5 @@
 $(function () {
-  // initialize canvas and context when able to
+// initialize canvas and context when able to
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
   window.addEventListener("load", loadJson);
@@ -31,17 +31,39 @@ $(function () {
 
 
     // TODO 2 - Create Platforms
-
-
-
+    createPlatform(500, 650, 100, 100, "blue")
+    createPlatform(600, 550, 100, 100, "green")
+    createPlatform(800, 500, 100, 100, "green")
+    createPlatform(900, 600, 100, 100, "blue")
+    createPlatform(700, 700, 100, 100, "pink")
+    createPlatform(700, 600, 100, 100, "pink")
+    createPlatform(700, 300, 100, 100,"pink")
+    createPlatform(900, 400, 100, 100, "green")
+    createPlatform(1000, 300, 100, 100, "blue")
+    createPlatform(1200, 700, 100, 100, "blue")
+    createPlatform(1200, 600, 100, 100, "blue")
+    createPlatform(1100, 700, 100, 100, "green")
+    createPlatform(1100, 600, 100, 100, "green")
+    createPlatform(1200, 500, 100, 100, "blue")
+    createPlatform(1300, 700, 100, 100, "red")
+    createPlatform(1300, 600, 100, 100, "red")
+    createPlatform(1300, 500, 100, 100, "red")
+    createPlatform(1300, 400, 100, 100, "red")
+    createBadPlatform(400, 700, 100, 70, "red")
+    createBadPlatform(1000, 700, 100, 100, "red")
 
     // TODO 3 - Create Collectables
-
+    createCollectable("max", 730, 150, 0.5 , 0.5)
+    createCollectable("steve", 920, 520, 0, 0)
+    createCollectable("grace", 730, 530, 0, 0)
 
 
     
     // TODO 4 - Create Cannons
-
+     createCannon("left", 500, 1000)
+     createCannon("right", 350, 1000)
+     createCannon("left", 300, 2000)
+     
 
     
     
